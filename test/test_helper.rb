8884da0a553b2e41debaec34f00fb2b9a5a0678b
Josh Washke <jwashke@icloud.com>
2016-03-01 13:40:20 -0700

    Finished Merchant Repository tests

---
+++ test/test_helper.rb
@@ -0,0 +1,14 @@
+
+class TestHelper 
+	def array_of_merchants
+		[Merchant.new({
+      :id   => 11111111,
+      :name => "Virgina Hams" }),
+     Merchant.new({
+      :id   => 55555555,
+      :name => "Turing School" }),
+     Merchant.new({
+      :id   => 33333333,
+      :name => "Hams and things" })]
+   end
+ end
