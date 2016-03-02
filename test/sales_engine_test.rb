4dcb2590ba523678d6949979698d31d348dad6b6
Deborah Hamel <deborahleehamel@gmail.com>
2016-03-02 15:29:51 -0700

    Iter0 - Added more sales_engine tests, all passing.

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -4,6 +4,7 @@ require_relative '../lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
   def setup
+    @test_helper = TestHelper.new
     @sales_engine = SalesEngine.new({ :merchants => [], :items => [] })
   end
 
@@ -19,4 +20,40 @@ class SalesEngineTest < Minitest::Test
     assert @sales_engine.merchants.instance_of?(MerchantRepository)
   end
 
+  def test_it_populates_item_repository_with_an_item_object
+    hash = @test_helper.sample_item_hash_info
+    assert @sales_engine.populate_item_repository(hash).items[0].instance_of?(Item)
+  end
+
+  def test_it_populates_item_repository_with_item_id
+    assert_equal @test_helper.sample_item_object[0].id, @sales_engine.populate_item_repository(@test_helper.sample_item_hash_info).items[0].id
+  end
+
+  def test_it_populates_item_repository_with_item_name
+    assert_equal @test_helper.sample_item_object[0].name, @sales_engine.populate_item_repository(@test_helper.sample_item_hash_info).items[0].name
+  end
+
+  def test_it_populates_item_repository_with_item_description
+    assert_equal @test_helper.sample_item_object[0].description, @sales_engine.populate_item_repository(@test_helper.sample_item_hash_info).items[0].description
+  end
+
+  def test_it_populates_item_repository_with_item_unit_price
+    assert_equal @test_helper.sample_item_object[0].unit_price, @sales_engine.populate_item_repository(@test_helper.sample_item_hash_info).items[0].unit_price
+  end
+
+  def test_it_populates_item_repository_with_item_merchant_id
+    assert_equal @test_helper.sample_item_object[0].merchant_id, @sales_engine.populate_item_repository(@test_helper.sample_item_hash_info).items[0].merchant_id
+  end
+
+  def test_it_populates_merchant_repository_with_a_merchant_object
+     assert @sales_engine.populate_merchant_repository(@test_helper.sample_merchant_hash_info).merchants[0].instance_of?(Merchant)
+  end
+
+  def test_it_populates_merchant_repository_with_a_merchant_id
+     assert_equal @test_helper.sample_merchant_object[0].id, @sales_engine.populate_merchant_repository(@test_helper.sample_merchant_hash_info).merchants[0].id
+  end
+
+  def test_it_populates_merchant_repository_with_a_merchant_name
+     assert_equal @test_helper.sample_merchant_object[0].name, @sales_engine.populate_merchant_repository(@test_helper.sample_merchant_hash_info).merchants[0].name
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,10 @@ require_relative '../lib/merchant'
 require_relative '../lib/item'
 
 class TestHelper
+	def initialize
+		@time = Time.now
+	end
+
 	def array_of_merchants
 		[Merchant.new({
       :id   => "11111111",
@@ -17,30 +21,64 @@ class TestHelper
    end
 
 	def array_of_items
-		time = Time.now
 		[Item.new({
       :id          => "263395237",
       :name        => "Country Ham",
       :description => "Delicious gift for grandpa",
       :unit_price  => BigDecimal.new(10.99,4),
       :merchant_id => "11111111",
-      :created_at  => time,
-      :updated_at  => time }),
+      :created_at  => @time,
+      :updated_at  => @time }),
 	  Item.new({
       :id          => "263395237",
       :name        => "Pencil",
 	    :description => "You can use it to write things",
 	    :unit_price  => BigDecimal.new(1.99,3),
 	    :merchant_id => "55555555",
-	    :created_at  => time,
-	    :updated_at  => time }),
+	    :created_at  => @time,
+	    :updated_at  => @time }),
 		Item.new({
 		  :id          => "263395237",
 		  :name        => "Sugar Cured Ham",
 		  :description => "Delicious sugary gift for grandma",
 		  :unit_price  => BigDecimal.new(15.99,4),
 		  :merchant_id => "33333333",
-		  :created_at  => time,
-		  :updated_at  => time })]
+		  :created_at  => @time,
+		  :updated_at  => @time })]
 	end
+
+	def sample_item_hash_info
+		[{
+      :id          => "263395237",
+      :name        => "Country Ham",
+      :description => "Delicious gift for grandpa",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :merchant_id => "11111111",
+      :created_at  => @time,
+      :updated_at  => @time }]
+	end
+
+	def sample_item_object
+		[Item.new({
+      :id          => "263395237",
+      :name        => "Country Ham",
+      :description => "Delicious gift for grandpa",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :merchant_id => "11111111",
+      :created_at  => @time,
+      :updated_at  => @time })]
+		end
+
+		def sample_merchant_hash_info
+			[{
+	      :id   => "11111111",
+	      :name => "Virginia Hams" }]
+		end
+
+		def sample_merchant_object
+			[Merchant.new({
+	      :id   => "11111111",
+	      :name => "Virginia Hams" })]
+			end
+
 end
